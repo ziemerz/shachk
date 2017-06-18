0d86c1553b0647c563027da02b78f3dbe2fe4d7f
Mikkel Ziemer <mzhjensen@gmail.com>
2017-06-18 02:11:24 +0200

    Initial commit

---
+++ shachk.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"log"
+	"io"
+	"crypto/sha256"
+	"fmt"
+	"encoding/hex"
+	"strings"
+	"net/http"
+	"io/ioutil"
+)
+
+func main() {
+	var fileFlag string
+	flag.StringVar(&fileFlag, "f", "filename", "Name of file to check shasum for")
+	flag.Parse()
+
+	hasher := sha256.New()
+
+
+	file, err := os.Open(fileFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	//Copy file to hasher
+	if _, err := io.Copy(hasher, file); err != nil {
+		log.Fatal(err)
+	}
+
+	fileSum := hex.EncodeToString(hasher.Sum(nil))
+
+	trueSum := os.Args[len(os.Args) -1]
+
+	if strings.ContainsAny(trueSum, "http | https") {
+		if resp, err := http.Get(trueSum); err != nil {
+			log.Fatal(err)
+		} else {
+			defer resp.Body.Close()
+			body, _  := ioutil.ReadAll(resp.Body)
+			trueSum = strings.Split(string(body), " ")[0]
+		}
+
+	}
+
+	if fileSum == trueSum {
+		fmt.Println("Sums are equal")
+	} else {
+		fmt.Println("WARNING! Sums are not equal")
+	}
+
+}
